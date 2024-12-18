a0e81706bf18bd79b098328f8437b8c740723abb
fcgorter <floriski@hotmail.nl>
2024-12-18 13:54:05 +0100

    scripts

---
+++ env.sh
@@ -0,0 +1,27 @@
+# Execute this script with: `source env.sh`
+
+# Change Me! depending on where you cloned the RSan repo
+export RSAN_TOP=/home/ubuntu/floris/rangesanitizer
+
+# Change Me! depending on where you installed SPEC CPU 2006
+export RSAN_SPEC2006=/home/ubuntu/floris/rangesanitizer/spec2006
+
+# LLVM/Clang
+export RSAN_LLVM=$RSAN_TOP/llvm-project-16/llvm
+export RSAN_LLVM_BUILD=$RSAN_TOP/llvm-build
+export RSAN_C=$RSAN_LLVM_BUILD/bin/clang
+export RSAN_CXX=$RSAN_LLVM_BUILD/bin/clang++
+
+# TCMalloc
+export RSAN_TC_BASE=$RSAN_TOP/tcmalloc-baseline
+export RSAN_TC_BASE_BUILD=$RSAN_TOP/tcmalloc-baseline-build
+export RSAN_TC_IMPL=$RSAN_TOP/tcmalloc-implicit
+export RSAN_TC_IMPL_BUILD=$RSAN_TOP/tcmalloc-impl-build
+export RSAN_TC_EXPL=$RSAN_TOP/tcmalloc-explicit
+export RSAN_TC_EXPL_BUILD=$RSAN_TOP/tcmalloc-expl-build
+
+# Infra
+export RSAN_INFRA=$RSAN_TOP/infra
+
+# Suggested for stable benchmarking (sudo)
+# echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
---
+++ install.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+set -e
+set -x
+
+ROOT_DIR=$(pwd)
+
+# Sanity check: was env.sh loaded?
+if [ -z $RSAN_C ]
+then
+    echo $RSAN_C
+    echo "Environment not set up! Execute source env.sh"
+    exit 1
+fi
+
+
+# Detect which architecture to build for
+ARCH=$(uname -m)
+TARGET=""
+echo "Detected system architecture: $ARCH"
+if [ $ARCH == "x86_64" ]; then
+    TARGET="X86"
+elif [ $ARCH == "aarch64" ]; then
+    TARGET="AArch64"
+else
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
+# Build LLVM if it does not yet exist
+if [[ ! -f $RSAN_C ]]
+then
+    mkdir -p $RSAN_LLVM_BUILD
+    cd $RSAN_LLVM_BUILD
+    cmake -DLLVM_ENABLE_PROJECTS="clang;lld" -DLLVM_ENABLE_RUNTIMES="compiler-rt" -DCMAKE_BUILD_TYPE=Release -GNinja -DLLVM_PARALLEL_LINK_JOBS=1 -DLLVM_TARGETS_TO_BUILD=$TARGET -DCLANG_ENABLE_STATIC_ANALYZER=OFF -DCLANG_ENABLE_ARCMT=OFF ../llvm-project-16/llvm
+    ninja
+fi
+
+# If after compilation we still do not have clang, exit
+if [[ ! -f $RSAN_C ]]
+then
+  echo "Clang was not successfully built... exit"
+  exit 1
+fi
+
+# Build TCMalloc baseline (clean TCMalloc v2.15)
+if [[ ! -f $RSAN_TC_BASE_BUILD/lib/libtcmalloc.a ]]; then
+    cd $ROOT_DIR
+    git clone https://github.com/gperftools/gperftools.git --branch gperftools-2.15 tcmalloc-baseline
+    cd $RSAN_TC_BASE
+    libtoolize
+    ./autogen.sh || true
+
+    mkdir -p $ROOT_DIR/tcmalloc-baseline-build
+    cd $RSAN_TC_BASE_BUILD
+    ../tcmalloc-baseline/configure --prefix=$ROOT_DIR/tcmalloc-baseline-build
+    make -j $(nproc)
+    make install
+fi
+
+# Build TCMalloc-implicit: can only be built on x86 (BMI support)
+if [ $ARCH == "x86_64" ] && [[ ! -f $RSAN_TC_IMPL_BUILD/lib/libtcmalloc.a ]]; then
+    cd $RSAN_TC_IMPL
+    libtoolize
+    echo "a...."
+    ./autogen.sh || true
+    echo "b..."
+
+    mkdir -p $RSAN_TC_IMPL_BUILD
+    cd $RSAN_TC_IMPL_BUILD
+    CFLAGS="-g -O2 -mbmi2" CXXFLAGS="-g -O2 -mbmi2" ../tcmalloc-implicit/configure --prefix=$RSAN_TC_IMPL_BUILD
+    make -j $(nproc)
+    make install
+
+# Build TCMalloc-explicit: currently assumes Arm TBI (Intel LAM support in a different RSan branch)
+elif [ $ARCH == "aarch64" ] && [[ ! -f $RSAN_TC_EXPL_BUILD/lib/libtcmalloc.a ]]; then
+    cd $RSAN_TC_EXPL
+    libtoolize
+    ./autogen.sh || true
+
+    mkdir -p $RSAN_TC_EXPL_BUILD
+    cd $RSAN_TC_EXPL_BUILD
+    ../tcmalloc-explicit/configure --prefix=$RSAN_TC_EXPL_BUILD
+    make -j $(nproc)
+    make install
+fi
+
+echo "All done"
+
